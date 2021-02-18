644c11a250fbb4718d0f310c706ce10e895af1ed
Subteniente <ron.subteniente@gmail.com>
2021-02-19 00:50:41 +0800

    Update md5 encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
 
 const User = mongoose.model("user", userSchema);
 
@@ -42,12 +42,12 @@ app.post("/register", function(req, res) {
     
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
         if (!err) {
-            res.render("secrets");
+            res.render("home");
         } else {
             console.log(err);
         }
@@ -57,7 +57,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser) {
         if (err) {
